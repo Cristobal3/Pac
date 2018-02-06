067c1a4e20dac5ad0e158b358cf5678828651c99
Cristobal <manzanares.cristobal@gmail.com>
2018-02-06 10:40:43 -0600

    Just another test

--- CSCI203/hello.py
+++ CSCI203/hello.py
@@ -1,8 +1,16 @@
 '''
+Problem 1
 name = raw_input("What is your name? ")
 print('Hello, %s!') %(name) 
-'''
 
+
+Problem 2
 name1 = input("WHAT IS YOUR NAME")
 print('HELLO, ', name1,'!')
 print('YOUR NAME HAS ',len(name1),' LETTERS IN IT! AWESOME!')
+'''
+#Problem 3
+
+
+
+
